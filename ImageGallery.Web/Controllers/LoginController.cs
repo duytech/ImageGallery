29834b4d792b63a625236fd01822b264530fe2e6
Duy <samanosuke146@gmail.com>
2016-03-14 21:39:31 +0700

    Commit for new project.

---
+++ ImageGallery.Web/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using ImageGallery.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageGallery.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn(User user)
+        {
+            if (user == null)
+                return null;
+
+            if (String.IsNullOrEmpty(user.Name) || String.IsNullOrEmpty(user.Password))
+                return null;
+
+            return View("Successful");
+        }
+    }
+}
---
+++ LoginApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginApp.Startup))]
+namespace LoginApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
